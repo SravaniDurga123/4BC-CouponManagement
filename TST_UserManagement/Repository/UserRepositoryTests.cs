f8bc698c2bb71767d01a1da52bdbbbd332dc0393
SravaniDurga123 <sravanidurga.maragani@gmail.com>
2020-05-07 09:48:44 +0530

    sixteen

--- TST_UserManagement/Repository/UserRepositoryTests.cs
+++ TST_UserManagement/Repository/UserRepositoryTests.cs
@@ -23,6 +23,10 @@ namespace CouponManagementTestCase.Repository
             userRepository = new UserRepository(mockCouponManagementContext);
             mockUserDatas = new UserDatas();
         }
+        /// <summary>
+        /// To Test the Get all users
+        /// </summary>
+        /// <returns></returns>
         [Test]
         public async Task GetAllUsers_Valid_Returns()
         {
@@ -33,6 +37,10 @@ namespace CouponManagementTestCase.Repository
             Assert.That(getAllUser.Count, Is.EqualTo(2));
 
         }
+        /// <summary>
+        /// To the User Register
+        /// </summary>
+        /// <returns></returns>
         [Test]
         public async Task UserRegister_valid_Returns()
         {
@@ -57,6 +65,10 @@ namespace CouponManagementTestCase.Repository
             Assert.That(getUserById, Is.EqualTo("true"));
 
         }
+        /// <summary>
+        ///     To test Get User 
+        /// </summary>
+        /// <returns></returns>
         [Test]
         public async Task GetUser_Valid_Returns()
         {
@@ -65,6 +77,22 @@ namespace CouponManagementTestCase.Repository
             var getUserById = await userRepository.GetUser(10);
             Assert.That(getUserById, Is.Not.Null);
             Assert.That(getUserById.UserId, Is.EqualTo(10));
+        }
+        /// <summary>
+        /// To Test the UpdateUser
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [Test]
+        public async Task UpdateUser_Valid_Returns(UserDetails user)
+        {
+            mockCouponManagementContext.UserDetails.AddRange(mockUserDatas.userDetails);
+            await mockCouponManagementContext.SaveChangesAsync();
+            var getUserById = await userRepository.GetUser(10);
+            user.EmailAddr = "hello@cognizant.com";
+            var updateUser = await userRepository.UpdateUser(user);
+            UserDetails user1 = await userRepository.GetUser(10);
+            Assert.AreSame(user, user1);
         }
 
     }
